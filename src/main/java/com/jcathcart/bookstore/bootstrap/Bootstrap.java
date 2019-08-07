f4b2ba8e5f5c91fd8c860872220a22ceb2ec2181
CathcartJL <jlcathcart@gmail.com>
2019-08-06 20:06:40 -0600

    Implemented bootstrap class. closes #1

--- src/main/java/com/jcathcart/bookstore/bootstrap/Bootstrap.java
+++ src/main/java/com/jcathcart/bookstore/bootstrap/Bootstrap.java
@@ -1,4 +1,21 @@
 package com.jcathcart.bookstore.bootstrap;
 
-public class Bootstrap {
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Bootstrap implements CommandLineRunner {
+
+    private final Logger log = LoggerFactory.getLogger(Bootstrap.class);
+
+    public Bootstrap() {
+        log.debug("I am Bootstrap");
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        log.debug("Bootstrap is on the run!");
+    }
 }
